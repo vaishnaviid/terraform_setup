cd82e184ec804f1408468783f66c0dd8e8635193
vaishnavi dhawalekar <vaishnavidhawalekar@gmail.com>
2025-11-20 16:31:54 +0530

    giit

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "ap-southeast-2"
+}   
+
+
+resource "aws_instance" "first_instance" {
+    ami           = "ami-007e5a061b93ceb2f"
+    instance_type = "t3.micro"
+    
+    tags = {
+        Name = "FirstInstance"
+    }
+  
+} 
+  
