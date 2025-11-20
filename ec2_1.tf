2d94caf1e69489cea99164e73663979edf7860e1
vaishnavi dhawalekar <vaishnavidhawalekar@gmail.com>
2025-11-20 18:16:24 +0530

    ec2_1.tf file added

---
+++ ec2_1.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+    region = "ap-southeast-2"
+}   
+
+
+resource "aws_instance" "create_instance" {
+    ami           = "ami-007e5a061b93ceb2f"
+    instance_type = "t3.micro"
+    count = 2
+    tags = {
+        Name = "FirstInstance"
+    }
+}
+
+resource "aws_instance" "create_instance" {
+    ami           = "ami-007e5a061b93ceb2f"
+    instance_type = "t3.micro"
+    count = 2
+    tags = {
+        Name = "secondInstance"
+    }
+}
+
+resource "aws_instance" "create_instance" {
+    ami           = "ami-007e5a061b93ceb2f"
+    instance_type = "t3.micro"
+    count = 2
+    tags = {
+        Name = "ThirdInstance"
+    }
+} 
+  
