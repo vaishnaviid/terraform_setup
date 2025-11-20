58780426fbd93590a823aa58a35f6622e9e93aab
vaishnavi dhawalekar <vaishnavidhawalekar@gmail.com>
2025-11-20 18:42:36 +0530

    code updated

--- ec2_1.tf
+++ ec2_1.tf
@@ -7,27 +7,11 @@ provider "aws" {
 resource "aws_instance" "create_instance3" {
     ami           = "ami-007e5a061b93ceb2f"
     instance_type = "t3.micro"
-    count = 2
+    count = 3
     tags = {
-        Name = "FirstInstance"
+        Name = "webserver${count.index + 1}"
     }
 }
 
-resource "aws_instance" "create_instance2" {
-    ami           = "ami-007e5a061b93ceb2f"
-    instance_type = "t3.micro"
-    count = 2
-    tags = {
-        Name = "secondInstance"
-    }
-}
 
-resource "aws_instance" "create_instance1" {
-    ami           = "ami-007e5a061b93ceb2f"
-    instance_type = "t3.micro"
-    count = 2
-    tags = {
-        Name = "ThirdInstance"
-    }
-} 
   
