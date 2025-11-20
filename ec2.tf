8d085afb8c1b103182809c1d975774e8880b5887
vaishnavi dhawalekar <vaishnavidhawalekar@gmail.com>
2025-11-20 18:10:21 +0530

    ec2.tf chnaged

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "first_instance" {
     ami           = "ami-007e5a061b93ceb2f"
     instance_type = "t3.micro"
-    
+    count = 2
     tags = {
         Name = "FirstInstance"
     }
