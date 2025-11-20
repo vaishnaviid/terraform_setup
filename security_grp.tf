bc089696e7f896ac9c4524f4a01c96e06561fdce
vaishnavi dhawalekar <vaishnavidhawalekar@gmail.com>
2025-11-20 23:47:07 +0530

    new sg file added

--- security_grp.tf
+++ security_grp.tf
@@ -6,8 +6,8 @@ provider "aws" {
 resource "aws_instance" "web" {
     ami = "ami-007e5a061b93ceb2f"
     instance_type = "t3.micro"
-    key_name = "3ter"
-    vpc_security_group_ids = "sgr-0c3c97ba54408deed"
+    key_name = "3tier"
+    vpc_security_group_ids = ["sgr-0c3c97ba54408deed"]
     tags = {
         Name = "mywebserver"
     }
