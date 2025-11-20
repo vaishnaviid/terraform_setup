397d9e9a6a7f0fa8363e9cdbc4833cee8a7db6f9
vaishnavi dhawalekar <vaishnavidhawalekar@gmail.com>
2025-11-20 23:42:35 +0530

    new sg file added

---
+++ security_grp.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "ap-southeast-2"
+}   
+
+resource "aws_instance" "web" {
+    ami = "ami-007e5a061b93ceb2f"
+    instance_type = "t3.micro"
+    key_name = "3ter"
+    vpc_security_group_ids = "sgr-0c3c97ba54408deed"
+    tags = {
+        Name = "mywebserver"
+    }
+}
