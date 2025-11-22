6dd75b00bd0407493e8f05fa197d82f4decfa590
vaishnavi dhawalekar <vaishnavidhawalekar@gmail.com>
2025-11-22 13:46:09 +0530

    new file added

--- Day05/state.tf
+++ Day05/state.tf
@@ -6,3 +6,18 @@ terraform {
     
   }
 }
+provider "aws" {
+    region = "ap-southeast-2"
+  
+}
+
+resource "aws_instance" "appserver" {
+  # refers to variables defined in variables.tf
+  ami = var.ami
+  instance_type = var.instance_type
+  key_name = var.key_name
+  vpc_security_group_ids = var.sec_grp
+  tags = {
+    Name = "appserver"
+  }
+}
